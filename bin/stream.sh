5e9604c102f4e4fc7cc3a6401d293f81bcbc4069
Julian Kern <mail@juliankern.com>
2017-04-23 02:36:01 +0200

    added comment to bash stream

--- bin/stream.sh
+++ bin/stream.sh
@@ -8,4 +8,5 @@
 # 7: targetVideoPort
 #
 
-raspivid -o - -w $1 -h $2 -fps $3 -b $(expr $4 \* 1000) | ffmpeg -re -f avfoundation -r 29.970000 -i 0:0 -threads 0 -vcodec libx264 -an -pix_fmt yuv420p -r $3 -f rawvideo -tune zerolatency -vf scale=$1:$2 -b:v $4k -bufsize $4k -payload_type 99 -ssrc 1 -f rtp -srtp_out_suite AES_CM_128_HMAC_SHA1_80 -srtp_out_params $5 srtp://$6:$7?rtcpport=$7&localrtcpport=$7&pkt_size=1378
+echo "raspivid -o - -w $1 -h $2 -fps $3 -b $(expr $4 \* 1000) | ffmpeg -re -f avfoundation -r 29.970000 -i 0:0 -threads 0 -vcodec libx264 -an -pix_fmt yuv420p -r $3 -f rawvideo -tune zerolatency -vf scale=$1:$2 -b:v $4k -bufsize $4k -payload_type 99 -ssrc 1 -f rtp -srtp_out_suite AES_CM_128_HMAC_SHA1_80 -srtp_out_params $5 srtp://$6:$7?rtcpport=$7&localrtcpport=$7&pkt_size=1378"
+sudo raspivid -o - -w $1 -h $2 -fps $3 -b $(expr $4 \* 1000) | ffmpeg -re -f avfoundation -r 29.970000 -i 0:0 -threads 0 -vcodec libx264 -an -pix_fmt yuv420p -r $3 -f rawvideo -tune zerolatency -vf scale=$1:$2 -b:v $4k -bufsize $4k -payload_type 99 -ssrc 1 -f rtp -srtp_out_suite AES_CM_128_HMAC_SHA1_80 -srtp_out_params $5 srtp://$6:$7?rtcpport=$7&localrtcpport=$7&pkt_size=1378
